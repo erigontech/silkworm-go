435dee4642d4d243cf1566e5e16183edbe5b90bb
battlmonstr <battlmonstr@users.noreply.github.com>
2023-12-05 17:10:52 +0100

    fix link warnings on macOS 14
    ld: warning: object file (...000010.o) was built for newer macOS version (14.0) than being linked (13.3)

--- link_macos_arm64.go
+++ link_macos_arm64.go
@@ -5,5 +5,4 @@ package silkworm_go
 // #cgo LDFLAGS: -lsilkworm_capi
 // #cgo LDFLAGS: -L${SRCDIR}/lib/macos_arm64
 // #cgo LDFLAGS: -Wl,-rpath ${SRCDIR}/lib/macos_arm64
-// #cgo LDFLAGS: -mmacosx-version-min=13.3
 import "C"
