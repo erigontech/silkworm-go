d4ec8a8bce96936a0cf8056dfafe790fa83ae486
battlmonstr <battlmonstr@users.noreply.github.com>
2023-12-14 13:35:30 +0100

    more rpath for libsilkworm search
    on Linux search relative to $ORIGIN (see https://stackoverflow.com/a/72577483/1009546 )
    on macOS it should search relative to @executable_path, but Go doesn't support it:
    https://github.com/golang/go/issues/40559

--- link_linux_arm64.go
+++ link_linux_arm64.go
@@ -4,5 +4,7 @@ package silkworm_go
 
 // #cgo LDFLAGS: -lsilkworm_capi
 // #cgo LDFLAGS: -L${SRCDIR}/lib/linux_arm64
+// #cgo LDFLAGS: -Wl,-rpath,$ORIGIN
+// #cgo LDFLAGS: -Wl,-rpath,$ORIGIN/../lib
 // #cgo LDFLAGS: -Wl,-rpath ${SRCDIR}/lib/linux_arm64
 import "C"
--- link_macos_x64.go
+++ link_macos_x64.go
@@ -4,5 +4,7 @@ package silkworm_go
 
 // #cgo LDFLAGS: -lsilkworm_capi
 // #cgo LDFLAGS: -L${SRCDIR}/lib/macos_x64
+// #cgo LDFLAGS: -Wl,-rpath,./
+// #cgo LDFLAGS: -Wl,-rpath,./../lib
 // #cgo LDFLAGS: -Wl,-rpath ${SRCDIR}/lib/macos_x64
 import "C"
